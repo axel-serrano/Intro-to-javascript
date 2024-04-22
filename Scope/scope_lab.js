e7a5356f7e151b9ed44b07220c5bd803960f594c
axel-serrano <axelinsh2003@gmail.com>
2024-04-22 12:18:17 -0400

    this is my tasks on my activity of JavaScript

--- Scope/scope_lab.js
+++ Scope/scope_lab.js
@@ -1,32 +1,46 @@
-// Global Scope
-var globalVar = "I'm a global variable"; 
-let globalLet = "I'm also global, but scoped with let";
-const globalConst = "I'm a global constant";
+// // Global Scope
+// var globalVar = "I'm a global variable"; 
+// let globalLet = "I'm also global, but scoped with let";
+// const globalConst = "I'm a global constant";
 
-{
-// Block Scope
-var blockVar = "I'm a block-scoped var";
-let blockLet = "I'm a block-scoped let";
-const blockConst = "I'm a block-scoped const";
-}
+// {
+// // Block Scope
+// var blockVar = "I'm a block-scoped var";
+// let blockLet = "I'm a block-scoped let";
+// const blockConst = "I'm a block-scoped const";
+// }
 
 // Global Scope test 
-console.log(globalVar);
-console.log(globalLet);
-console.log(globalConst);
+// console.log(globalVar);
+// console.log(globalLet);
+// console.log(globalConst);
 
 //Block Scope test
 //console.log(blockVar);
 //console.log(blockLet);
 
-function show(){
-    var functionVar = "I'm a block-scoped var";
-    let functionLet = "I'm a block-scoped let";
-    const functionConst = "I'm a block-scoped const";
+// //function show(){
+//     var functionVar = "I'm a block-scoped var";
+//     let functionLet = "I'm a block-scoped let";
+//     const functionConst = "I'm a block-scoped const";
+// //}
+
+//show();
+
+// console.log(functionVar);
+// console.log(functionLet);
+// console.log(functionConst);
+
+{
+    var firstVar = "this is my first variable that I declare on JavaScript";
+    let num1 = 1;
+    const hi = 3.1416;
 }
 
-show();
+var firstVar = "this is my first variable that I declare on JavaScript";
+let num1 = 1;
+const hi = 3.1416;
 
-console.log(functionVar);
-console.log(functionLet);
-console.log(functionConst);
+console.log(firstVar);
+console.log(num1);
+console.log(hi);
