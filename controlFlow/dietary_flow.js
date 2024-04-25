e2d0cdfad4951e9f604fc40520860e63746a5130
axel-serrano <axelinsh2003@gmail.com>
2024-04-25 15:31:18 -0400

    this is my practice of workflow on javascript

---
+++ controlFlow/dietary_flow.js
@@ -0,0 +1,25 @@
+let userRole = prompt("Ingrese su nivel de membresia");
+let accesslevel;
+let isAuthenticated = true;
+let authenticationStatus = isAuthenticated ? "Authenticated" : "Not Authenticated";
+
+if (userRole === "employee"){
+    accesslevel = "Welcome Employee, you have access to all Dietary Services";
+} else if (userRole === "enrolled member"){
+    accesslevel = "Welcome Member, you have access to all Dietary Services and one-on-one interaction with a dietician";
+} else if (userRole === "subscriber"){
+    accesslevel = "Welcome Subscriber, you have partial access to facilitate the Dietary Services only";
+} else {
+    accesslevel = "you're a non-subcriber, please subscribe for access to all benefits";
+    userRole = prompt("Hello actually you don't have a subscription, \nPlease write Member or Subscriber depending on the type of member you want");
+    isAuthenticated = false;
+    if (userRole == "member" || "subscriber"){
+        accesslevel = `Now you are enrolled like ${userRole} enjoy with all your benefits`;
+        isAuthenticated = true;
+    } else if (userRole = ""){
+        alert("sorry, leave us your coment for why reason you don't want subscribe")
+    }
+}
+
+console.log("Access level:" + accesslevel);
+console.log("Authentication Status: " + authenticationStatus);
