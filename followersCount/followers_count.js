f1c945bbe2117deb738754f5507ad4f4a75c5e92
axel-serrano <axelinsh2003@gmail.com>
2024-04-30 21:49:12 -0400

    added a counter of followers with a checker that trigger alerts of congratulations

---
+++ followersCount/followers_count.js
@@ -0,0 +1,24 @@
+// the counter for the number of followers 
+let count = 0; // initialize count to 0
+
+function increaseCount(){
+    count++;
+    displayCount();
+    checkCountValue();
+}
+
+function displayCount(){
+    document.getElementById('countDisplay').innerHTML = count;
+}
+
+// check the value of count and trigger alerts 
+
+function checkCountValue(){
+
+    if (count === 10){
+        alert("Your Instagram post gained 10 followers! Congratulations!");
+    } else if (count === 20 ){
+        alert("Your Instagram post gained 20 followers! Keep it up!");
+    }
+
+}
